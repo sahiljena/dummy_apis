fc8ddea1f99b618748fbfb4f154e5fc54fd02fb4
sahiljena <sahiljena@protonmail.com>
2021-06-15 11:58:25 +0530

    Add landing page at '\'

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask,request,jsonify
+from flask import Flask,request,jsonify,render_template
 import json
 app = Flask(__name__)
 
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 @app.route('/route1',methods=['POST','GET'])
 def route1():
     return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
